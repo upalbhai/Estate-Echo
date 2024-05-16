a82ebdc51990386cf92735f4c016544b79127211
upalbhai <108395707+upalbhai@users.noreply.github.com>
2024-05-16 15:12:40 +0530

    Update SignIn.jsx

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -52,7 +52,7 @@ const SignIn = () => {
       <form onSubmit={submitHandler} className='flex flex-col gap-4' >
         <input type="email" placeholder='email' className='border p-3 rounded-lg' id='email' onChange={handleChange} required/>
         <input type="password" placeholder='password' className='border p-3 rounded-lg' id='password' onChange={handleChange} required/>
-        <button disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>{loading ? 'Loading...' :'Sign Up'}</button>
+        <button disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>{loading ? 'Loading...' :'Sign In'}</button>
         <Oath />
       </form>
       <div className='flex gap-2 mt-5'>
